6af3ba794b9f1cb13b159ecc65ae2a7502206d9a
vmk1315 <mewbow1315@gmail.com>
2020-01-07 20:48:45 +0530

    Added chat screen

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,18 +5,22 @@
       wrap
       class="col-12 row"
     >
-  <mainLogo class="col-7" :spinnerType="sType"></mainLogo>
+  <mainLogo class="col-7 animated fadeIn slower" :spinnerType="sType"></mainLogo>
   <div class="col-5" style="padding-top: 5.5em;">
-    <span class="font-weight-light" style="font-size: 2.1em;">Hi</span>
+    <span class="font-weight-light" style="font-size: 2.1em;">FRIDAY <span style="font-size:0.5em;" class="red--text">(Offline)</span></span>
     </div>
     </v-layout>
+    <!-- <v-layout> -->
+      <chats/>
+    <!-- </v-layout> -->
   </v-container>
 </template>
 
 <script>
 import mainLogo from './mainLogo'
+import chats from './chats'
 export default {
-  components:{mainLogo},
+  components:{mainLogo,chats},
 
   data: () => ({
     sType:'slow'
---
+++ src/components/chats.vue
@@ -0,0 +1,126 @@
+<template>
+  <div>
+    <!-- message body -->
+    <div :style="`min-height:${dwdth};max-height:${dwdth};overflow-x: hidden;overflow-y: auto;`">
+        <br>
+        <span v-for="(chat,index) in chatBody" v-bind:key="'i'+index">
+      <div :class="chat.isSystem?'chat-left-container':'chat-right-container'">
+          <div :class="chat.isSystem?'mb-1 mr-2 mt-1 pl-3 pr-3 pt-1 pb-1 purple chat-left animated fadeInLeft':'mb-1 mr-2 mt-1 pl-3 pr-3 pt-1 pb-1 purple darken-2 chat-right animated fadeInRight'">
+              {{chat.message}} 
+          </div>
+      </div>
+        </span>
+        <!-- outbox -->
+        <span v-for="(item,index) in outbox" v-bind:key="'o'+index">
+      <div class="chat-right-container">
+          <div class="mb-1 mr-2 mt-1 pl-3 pr-3 pt-1 pb-1 chat-sending chat-right animated fadeInRight">
+              {{item.message}} 
+          </div>
+      </div>
+        </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dwdth: 0,
+      outMessage:"",
+      chatBody:[
+          {
+              message:"hello",
+              isSystem:false
+          },
+          {
+              message:"I'm sorry, but what does that mean?",
+              isSystem:true
+          },
+          {
+              message:"It is a greeting",
+              isSystem:false
+          },
+          {
+              message:"Understood! Hello is a greeting.",
+              isSystem:true
+          },
+          {
+              message:"**Started learning all kinds of greetings from the internet**",
+              isSystem:true
+          },
+          {
+              message:"**Learning greetings (50% done)**",
+              isSystem:true
+          },
+          {
+              message:"Hello Friday",
+              isSystem:false
+          },
+          {
+              message:"Hi! How are you?",
+              isSystem:true
+          },
+          {
+              message:"**Learning greetings (80% done)**",
+              isSystem:true
+          },
+          {
+              message:"**Learning greetings (complete)**",
+              isSystem:true
+          },
+      ],
+      outbox:[]
+    };
+  },
+  mounted() {
+    this.dwdth = window.innerHeight - 300 + "px";
+    // this.dwdth="calc(100vh - 100px)"
+  },
+  methods:{
+      sendMessage(){
+          this.outbox=[...this.outbox,{message:this.outMessage}]
+          this.outMessage=""
+        //   delete the below when providing actual code
+          setTimeout(() => {
+              this.chatBody=[...this.chatBody,...this.outbox]
+              this.outbox=[]
+          }, 1500);
+        //   delete the above when providing actual code
+      }
+  }
+};
+</script>
+
+<style scoped>
+.chat-left-container{
+    display: flex;
+    flex-direction: row;
+}
+.chat-right-container{
+    display: flex;
+    flex-direction: row-reverse;
+}
+.chat-left{
+    border-radius: 25px;
+    font-size: medium;
+    font-weight: bold;
+    max-width: 50%;
+}
+.chat-right{
+    border-radius: 25px;
+    font-size: medium;
+    font-weight: bold;
+    max-width: 50%;    
+}
+.chat-sending{
+    background: #afafaf !important;
+    color: #ffffff !important;
+}
+.chat-sending::after {
+  content: " Sending...";
+  font-weight: normal;
+  font-size: 10px;
+  position: relative;
+}
+</style>
